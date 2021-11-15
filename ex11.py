ea873fc2189d56560503465ba2e225281daacd0d
rstoyanov <stoyanov.radostin@gmail.com>
2021-11-15 21:52:18 +0200

    ex9 to 13

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you?", end=' ') # end= says the next line to stays on the same line
+age = input()
+print("How tall are you", end=' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you are {age} old, {height} tall and {weight} havy.")
+
+
+
---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv # argument variables can be sent to the script for the command line
+# read the WYSS section for how to run this
+script, first, second, third = argv # script is always defined, it is not a parameter
+
+print("The script is called:",script)
+print("Your first variable is",first)
+print("Your second variable is",second)
+print("Your third variable is",third)
